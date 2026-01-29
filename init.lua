d2e5ec7ae50043799487f8da57927c34802a7c62
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-01-29 19:59:36 +0400

    bye

--- init.lua
+++ init.lua
@@ -1,10 +1,9 @@
 minetest.register_on_mods_loaded(function()
 	for node,def in pairs(minetest.registered_nodes) do
-		if def.papyrus > 0 then
-			minetest.override_item(node,{
-				walkable = false,
-				climbable = true,
-			})
+		minetest.override_item(node,{
+			walkable = false,
+			climbable = true,
+		})
 		end
 	end
 end)
