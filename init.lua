df720a4a1a54dff370bc700cf3dcb55647904f15
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-01-31 08:49:04 +0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,10 +1,5 @@
-minetest.register_on_mods_loaded(function()
-	for node,def in pairs(minetest.registered_nodes) do
-		if papyrus > 0 then
-			minetest.override_item(node,{
-				walkable = false,
-				climbable = true,
-			})
-		end
-	end
-end)
+local resistance = tonumber(minetest.settings:get("papyrusfix_move_resistance") or 2)
+core.override_item("default:papyrus",{
+	walkable = false,
+	move_resistance = resistance
+})
