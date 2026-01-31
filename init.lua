6a20ea6ccb39775e6a78cddb58fa01d8d94e705b
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-01-31 08:49:44 +0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
-local resistance = tonumber(minetest.settings:get("papyrusfix_move_resistance") or 2)
 core.override_item("default:papyrus",{
 	walkable = false,
-	move_resistance = resistance
+	climbable = true
 })
